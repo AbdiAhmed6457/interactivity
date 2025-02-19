6de750f9065fb88091a1517944a9a4599d798b77
AbdiAhmed6457 <samuelahmed6457@gmail.com>
2025-02-18 18:16:02 -0800

    decrease

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ document.getElementById(`increase`).onclick = function () {
     count++;
     document.getElementById(`counterLabel`).innerHTML = count;
 }
+
+document.getElementById(`decrease`).onclick = function() {
+    count--;
+    document.getElementById(`counterLabel`).innerHTML = count;
+}
+document.getElementById(`decrease`).onclick = function() {
+    count--;
+    document.getElementById(`counterLabel`).innerHTML = count;
+}
